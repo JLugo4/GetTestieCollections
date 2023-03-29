85de91852cc84f0105994748c359624a183626eb
Jarek <jarek@Zipcoders-MacBook-Pro-65.local>
2023-03-28 23:54:39 -0400

    6 Tests done

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -51,5 +51,41 @@ public class TestStack {
 
         Assert.assertTrue(map.containsKey("David") && map.containsValue(1));
     }
+    @Test
+    public void testLinkedList(){
+        LinkedList<Integer> Seth = new LinkedList<>();
+        Seth.add(1);
+
+        Assert.assertTrue(Seth.contains(1));
+    }
+    @Test
+    public void testDeque(){
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.add(1);
+        deque.add(2);
+        deque.add(3);
+        Integer expected = 3;
+
+        Assert.assertEquals(expected, deque.getLast());
+    }
+
+    @Test
+    public void testVector(){
+        Vector<String> vec = new Vector<>();
+        vec.add("Tim");
+        vec.add("Jim");
+        vec.add("Kim");
+        vec.add("Tim1");
+        vec.add("Jim1");
+        vec.add("Kim1");
+        vec.add("Tim2");
+        vec.add("Jim2");
+        vec.add("Kim2");
+        vec.add("Tim3");
+        vec.add("Jim3");
+        int expected = 20;
+        // Equals 20 because Vector grows capcity in increments of 10
+        Assert.assertEquals(expected, vec.capacity());
+    }
     
 }
